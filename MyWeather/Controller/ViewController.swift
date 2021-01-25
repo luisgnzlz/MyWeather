163df11c47a697ceea550baeaab7694cc3f24bcf
Luis Gonzalez <luisgonzalez@luiss-air.lan>
2021-01-25 15:48:13 -0800

    fixed issue with city and state name

--- MyWeather/Controller/ViewController.swift
+++ MyWeather/Controller/ViewController.swift
@@ -30,51 +30,48 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
            }
     
-    func didTabButton(progressNumber: Float, weather: WeatherResponse) {
-        mainView.mainTemp.mainTemp.text = "\(weather.main.temp)"
-       // mainView.mainTemp.cityName.text = "\()"
-       // mainView.mainTemp.countryName.text = "\()"
+    func didTabButton(progressNumber: Float, weather: WeatherResponse, cityName: String, stateName: String) {
+        let currentTemp = Int(weather.main.temp.rounded())
+        mainView.mainTemp.mainTemp.text = "\(currentTemp)°"
+        mainView.mainTemp.cityAndState.text = "\(cityName),\(stateName)"
         
         let progNum = progressNumber/100
         UIView.animate(withDuration: 4) {
-            print("this is a good thing")
             self.mainView.otherWeatherInfo.humidityBar.setProgress(progNum, animated: true)
         }
         self.mainView.otherWeatherInfo.humidityNumber.text = "\(weather.main.humidity)%"
         
     }
            
-           func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-               guard let locationValue : CLLocationCoordinate2D = manager.location?.coordinate else {
-                   return
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locationValue : CLLocationCoordinate2D = manager.location?.coordinate else {
+            return
                }
                
-               let lat = String(locationValue.latitude)
-               let longi = String(locationValue.longitude)
-               CLGeocoder().reverseGeocodeLocation(locations[0]) { (placemark, error) in
-                   if error != nil {
-                       print("Error")
-                   }
-                   else {
-                       if let place = placemark?[0] {
-                           guard let cityname = place.locality, let statename = place.administrativeArea else {
-                               return
-                           }
+        let lat = String(locationValue.latitude)
+        let longi = String(locationValue.longitude)
+            
+        CLGeocoder().reverseGeocodeLocation(locations[0]) { (placemark, error) in
+            if error != nil {
+                print("Error1111")
+            }
+            else {
+                if let place = placemark?[0] {
+                    guard let cityname = place.locality, let statename = place.administrativeArea else {
+                        return
+                    }
                            
-                           let setWeatherInfo:(WeatherResponse) -> Void = { currentWeather in
+            let setWeatherInfo:(WeatherResponse) -> Void = { currentWeather in
                                
-                               DispatchQueue.main.async {
-                                print("start")
-                                self.didTabButton(progressNumber: Float(currentWeather.main.temp), weather: currentWeather)
-                                print("end")
-                                   //self.displayWeatherInfo(currentWeather: currentWeather)
-                               }
-                           }
-                           self.apiWeather.weatherInfo(longitude: longi, latitude: lat, onCompletion: setWeatherInfo)
-                       }
-                   }
+    DispatchQueue.main.async {
+            self.didTabButton(progressNumber: Float(currentWeather.main.temp), weather: currentWeather, cityName: cityname, stateName: statename)
+    }
+        }
+                    self.apiWeather.weatherInfo(longitude: longi, latitude: lat, onCompletion: setWeatherInfo)
+                }
+            }
 
-               }
-           }
+        }
+    }
 }
 
