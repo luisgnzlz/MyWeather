3c5d671b01b38f1267ced1a1f1a6e683145b31d2
Luis Gonzalez <luisgonzalez@luiss-air.lan>
2021-02-01 17:16:18 -0800

    added modifications

--- MyWeather/View/ForcastCollectionViewController.swift
+++ MyWeather/View/ForcastCollectionViewController.swift
@@ -37,20 +37,20 @@ class ForcastCollectionViewController: UICollectionViewController {
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return 2
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
     
         // Configure the cell
-    
+        cell.backgroundColor = .green
         return cell
     }
 
