ec5369c307d5e379afbb42e6ddc5a53e39f9a456
Luis Gonzalez <luisgonzalez@luiss-air.lan>
2021-02-09 21:13:33 -0800

    updated for the 5 day forcast

--- MyWeather/View/FiveDayForcast.swift
+++ MyWeather/View/FiveDayForcast.swift
@@ -7,8 +7,35 @@
 
 import UIKit
 
-class FiveDayForcast: UICollectionView {
-
+class FiveDayForcast: UICollectionView, UICollectionViewDataSource {
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewFlowLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+        
+        let forecastLayout = UICollectionViewFlowLayout()
+        forecastLayout.scrollDirection = .horizontal
+        forecastLayout.itemSize = CGSize(width: 100, height: 100)
+        collectionViewLayout = forecastLayout
+        
+        self.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "collectionViewCell")
+        
+        
+        self.dataSource = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let collectionCell = self.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath)
+        collectionCell.backgroundColor = .red
+        return collectionCell
+    }
     
 
+    
 }
