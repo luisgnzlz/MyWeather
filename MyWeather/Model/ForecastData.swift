a038ce6a7c34efac97cbcc15db25474ee9402532
Luis Gonzalez <luisgonzalez@luiss-air.lan>
2021-02-15 19:29:28 -0800

    modifing the Forecast with a new file

---
+++ MyWeather/Model/ForecastData.swift
@@ -0,0 +1,20 @@
+//
+//  ForecastData.swift
+//  MyWeather
+//
+//  Created by Luis Gonzalez on 2/15/21.
+//
+
+import Foundation
+
+struct ForecastData {
+    var main = Double()
+    
+    init(mainWeather: Double) {
+        self.main = mainWeather
+    }
+    
+    init() {
+        
+    }
+}
